a99d49d4a92b5acfc995fc8f54fbe6722ddfd514
Zubin <zeuslawyer@users.noreply.github.com>
2021-12-08 04:58:22 +0000

    Add Box and Auth contracts. Add hardhat.

---
+++ contracts/Box.sol
@@ -0,0 +1,30 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "./access-control/Auth.sol";
+
+contract Box {
+    uint256 private _value;
+    Auth private _auth;
+
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 value);
+
+    constructor(){
+      _auth = new Auth(msg.sender);
+    }
+
+    // Stores a new value in the contract
+    function store(uint256 value) public {
+        require(_auth.isAdministrator(msg.sender), "Unauthorized to store");
+
+        _value = value;
+        emit ValueChanged(value);
+    }
+
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
